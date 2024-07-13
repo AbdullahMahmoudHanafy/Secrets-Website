3f6bc4ef9b9acd52153490df391f834733338e02
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-07-13 15:47:28 +0300

    final push

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/", async (req, res) => {
+
+    try {
+        const response = await axios.get('https://secrets-api.appbrewery.com/random');
+        let secret = response.data.secret, user = response.data.username;
+
+        res.render("index.ejs", {secret: secret, user: user});
+    } catch (error) {
+        console.error(error);
+    }
+})
+
 app.listen(port, console.log(`working on port number ${port}`));
